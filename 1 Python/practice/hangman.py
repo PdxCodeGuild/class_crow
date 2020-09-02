5e0f97f2d3c1ecb75a2a0823368e908cd866c82b
jpchato <jpchato@gmail.com>
2020-09-01 20:44:19 -0700

    completed hangman with class

--- 1 Python/practice/hangman.py
+++ 1 Python/practice/hangman.py
@@ -1,8 +1,8 @@
 import random
 
 def word_chooser():
-    open('english.txt', 'r')
-    f = open('english.txt', 'r')
+    open('/home/jpchato/class_crow/Code/jesse_pena/lectures/english.txt', 'r')
+    f = open('/home/jpchato/class_crow/Code/jesse_pena/lectures/english.txt', 'r')
     word_list = []
     words = f.read().split('\n')
     for word in words:
@@ -19,15 +19,30 @@ def hangman():
         compare.append('_')
     guesses = []
     while counter < 10:
+        print(random_word)
         counter += 1
-        guess = input('Whats your guess?')
+        guess = input('Whats your guess? ')
         if guess in guesses:
-            guess = input('You already guessed that! Try again.')
+            guess = input('You already guessed that! Try again. ')
         guesses.append(guess)
         for i in range(len(random_word)):
             if guess == random_word[i]:
                 compare[i] = guess 
-        print(compare)
+                # print(random_word)
+                # print(''.join(compare))
+                # if random_word == ''.join(compare):
+                #     print('Success')
+                if '_' not in compare:
+                    print('success')
+                    counter = 10
+        # print(compare)
+    print('You\'ve reached 10 guesses')
+    print(random_word)
+    play_again = input('Do you want to play again? ')
+    play_again.lower()
+    if play_again == 'yes' or 'y':
+        hangman()
+
 
 hangman()           
 
