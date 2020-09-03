8066272706d8adc644590e9dd9c68cc2b8325df5
jpchato <jpchato@gmail.com>
2020-09-02 21:15:50 -0700

    completed part 4 of lab 9

--- Code/jesse_pena/labs/lab_9.py
+++ Code/jesse_pena/labs/lab_9.py
@@ -23,45 +23,23 @@ def units_to_meters():
 
     print('The conversion to meters is ' + str(total))
 
+
 def users_choice():
-    feet_to_any = {'feet': 1, 'mile': 0.000189394, 'kilometer': 0.0003048, 'meter': 0.3048, 'yard': 0.9144, 'inch': 12}
+    conversion_dict = {'feet': 0.3048, 'mile': 1609.34, 'kilometer': 1000, 'meter': 1, 'yard': 0.9144, 'inch': 0.0254}
+    units_from = input('Which unit do you need to convert from? feet, mile, kilometer, meter, yard, or inch? ')
+    units_to = input('Which unit do you need to convert to? feet, mile, kilometer, meter, yard, or inch? ')
+    length = input ('What\'s the distance to convert? ')
 
-    miles_to_any = {'feet': 5280, 'mile': 1, 'kilometer': 1.60934, 'meter': 1609.34, 'yard': 1760, 'inch': 63360}
+    conversion_to_meters = conversion_dict.get(units_from)
+    print(conversion_to_meters)
+    print(length)
+    distance_in_meters = float(length) * float(conversion_to_meters)
+    print(distance_in_meters)
+    answer =  distance_in_meters / conversion_dict.get(units_to)
+    print(f'Distance of {length} {units_from} to {units_to} is {answer}')
 
-    km_to_any = {'feet': 3280.84, 'mile': 0.621371, 'kilometer': 1, 'meter': 1000, 'yard': 1093.61, 'inch': 39370.1}
-
-    meters_to_any = {'feet': 0.3048, 'mile': 1609.34, 'kilometer': 1000, 'meter': 1, 'yard': 0.9144, 'inch': 0.0254}
-
-    yards_to_any = {'feet': 3, 'mile': 0.000568182, 'kilometer': 0.0009144, 'meter': 0.91441, 'yard': 1, 'inch': 36}
-
-    inches_to_any = {'feet': 0.0833333, 'mile': 0.0000157828, 'kilometer': 0.0000254, 'meter': 0.0254, 'yard': 0.0277778, 'inch': 1}
-
-    starting_units = input('Which units are we converting from? feet, mile, kilometer, meter, yard, or inch?')
-
-    ending_units = input('Which units are we converting to? feet, mile, kilometer, meter, yard, or inch? ')
-
-    distance = int(input('What\'s the distance we\'re converting? '))
-
-    if starting_units == 'feet':
-        starting_conversion = feet_to_any.get(starting_units)
-    elif starting_units == 'mile':
-        starting_conversion = miles_to_any.get(starting_units)
-    elif starting_units == 'kilometer':
-        starting_conversion = km_to_any.get(starting_units)
-    print(starting_conversion)
-
-
-    
-
-    
-    # ending_conversion = conversion_dict.get(ending_units)
-
-    # print(f'{starting_conversion} {ending_conversion}  {distance}')
-    # answer = distance*starting_conversion*ending_conversion
-
-    # print(answer)
 
 if __name__ == "__main__":
-    # feet_to_meters()
-    # units_to_meters()
+    feet_to_meters()
+    units_to_meters()
     users_choice()
