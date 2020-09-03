ba96da18a963245fb6f7957a9663fc41c20eff3a
sbtries <sarahbtracy17@gmail.com>
2020-09-02 18:04:36 -0700

    adding hangman solution

--- 1 Python/practice/hangman.py
+++ 1 Python/practice/hangman.py
@@ -1,14 +1,16 @@
-import random
-open('english.txt', 'r')
-f = open('english.txt', 'r')
-word_list = []
-words = f.read().split('\n')
-for word in words:
-    if len(word) <= 5:
-        word_list.append(word)
-​
-random_word = random.choice(word_list)
+# import random
+# open('english.txt', 'r')
+# f = open('english.txt', 'r')
+# word_list = []
+# words = f.read().split('\n')
+# for word in words:
+#     if len(word) <= 5:
+#         word_list.append(word)
+# ​
+# random_word = random.choice(word_list)
 
 
+list = ['1','2']
 
-
+hello = ''.join(list)
+print(hello)
