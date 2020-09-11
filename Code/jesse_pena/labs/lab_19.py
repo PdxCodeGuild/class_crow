02be7c37b58793fc37eb0ff25d087acc96877114
jpchato <jpchato@gmail.com>
2020-09-10 19:14:32 -0700

    lab19 complete"

--- Code/jesse_pena/labs/lab_19.py
+++ Code/jesse_pena/labs/lab_19.py
@@ -58,27 +58,106 @@ def blackjack_aces():
     card_2 = input('What\'s your second card?' ).upper()
     card_3 = input('What\'s your third card?' ).upper()
     hand.append(card_1)
+    hand.append(card_2)
+    hand.append(card_3)
 
     # card_1 = deck['A']
     # card_2 = deck['2']
     # card_3 = deck['3']
 
     initial_hand = deck.get(card_1) + deck.get(card_2) + deck.get(card_3)
+    print(hand)
 
     if 'A' in hand:
-        print('hi')
+        deck['A'] = 11
+        initial_hand = deck.get(card_1) + deck.get(card_2) + deck.get(card_3)
+        # print(initial_hand)
+        if initial_hand == 33:
+            initial_hand = 13
+            print(initial_hand, 'hit')
 
-    if int(initial_hand) < 17:
-        print('Hit')
-        
-    elif 21 > initial_hand >= 17:
-        print('Stay')
+        elif initial_hand == 32:
+            initial_hand = 12
+            print(initial_hand, 'hit')
 
-    elif initial_hand == 21:
-        print('Blackjack')
+        elif initial_hand == 31:
+            initial_hand = 21
+            print(initial_hand, 'Blackjack')
+
+        elif initial_hand == 30:
+            initial_hand = 20
+            print(initial_hand, 'Stay')
+
+        elif initial_hand == 29:
+            initial_hand = 19
+            print(initial_hand, 'Stay')
+
+        elif initial_hand == 28:
+            initial_hand = 18
+            print(initial_hand, 'Stay')
+
+        elif initial_hand == 27:
+            initial_hand = 17
+            print(initial_hand, 'Stay')
+
+        elif initial_hand == 26:
+            initial_hand = 16
+            print(initial_hand, 'Hit')
+
+        elif initial_hand == 25:
+            initial_hand = 15
+            print(initial_hand, 'Hit')
+
+        elif initial_hand == 24:
+            initial_hand = 14
+            print(initial_hand, 'Hit')
+
+        elif initial_hand == 21:
+            print(initial_hand, 'Blackjack')
+
+        elif initial_hand < 21:
+
+            if int(initial_hand) < 17:
+                print(initial_hand, 'Hit')
+            
+            elif 21 > initial_hand >= 17:
+                print(initial_hand, 'Stay')
+
+            elif initial_hand == 21:
+                print(initial_hand, 'Blackjack')
+            
+            elif initial_hand > 21:
+                print(initial_hand, 'Bust')
+
+        elif 30 > initial_hand > 21:
+
+            deck['A'] = 1
+            initial_hand = deck.get(card_1) + deck.get(card_2) + deck.get(card_3)
+
+            if int(initial_hand) < 17:
+                print(initial_hand, 'Hit')
+            
+                if 21 > initial_hand >= 17:
+                    print(initial_hand, 'Stay')
+
+                elif initial_hand == 21:
+                    print(initial_hand, 'Blackjack')
+                    
+                elif initial_hand > 21:
+                    print(initial_hand, 'Bust')
+    elif 'A' not in hand:
         
-    elif initial_hand > 21:
-        print('Bust')
+        if int(initial_hand) < 17:
+            print(initial_hand, 'Hit')
+            
+        elif 21 > initial_hand >= 17:
+            print(initial_hand, 'Stay')
+
+        elif initial_hand == 21:
+            print(initial_hand, 'Blackjack')
+            
+        elif initial_hand > 21:
+            print(initial_hand, 'Bust')
 
 
 if __name__ == "__main__":
