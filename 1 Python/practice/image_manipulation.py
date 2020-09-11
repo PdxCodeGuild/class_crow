7130ae3f9c3a795cab23ba3f3376a9dca145f837
sbtries <49175343+sbtries@users.noreply.github.com>
2020-09-10 18:09:30 -0700

    Update image_manipulation.py

--- 1 Python/practice/image_manipulation.py
+++ 1 Python/practice/image_manipulation.py
@@ -6,6 +6,8 @@ from PIL import Image
 img = Image.open("corgi.jpg") # must be in same folder
 width, height = img.size
 pixels = img.load()
+
+#filter portion here:
 for x in range(width):
   for y in range(height):
     r, g, b = pixels[x, y]
