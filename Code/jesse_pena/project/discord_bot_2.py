75e6d65df7a128e84300f15dab33adfd47d247f6
jpchato <jpchato@gmail.com>
2020-09-25 21:46:10 -0700

    working discord bot with connection to opgg

--- Code/jesse_pena/project/discord_bot_2.py
+++ Code/jesse_pena/project/discord_bot_2.py
@@ -1,20 +1,19 @@
-# bot.py
 import os
 import random
 from dotenv import load_dotenv
 import discord
 import requests
 from bs4 import BeautifulSoup
-
-#1
 from discord.ext import commands
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 client = discord.Client()
-
-#2
 bot = commands.Bot(command_prefix='!')
+
+
+# reference: https://realpython.com/python-requests/
+# reference: https://realpython.com/how-to-make-a-discord-bot-python/
 
 @bot.event
 async def on_ready():
@@ -73,26 +72,46 @@ async def on_error(event, *args, **kwargs):
 
 @bot.command(name='opgg', help='Looks up user\'s op.gg')
 async def op_gg(ctx, summoner_name):
-    response = requests.get('https://na.op.gg/summoner/userName={summoner_name}')
+    # champ_lookup(summoner_name)
+    # print(summoner_name)
 
-    response = requests.get('https://na.op.gg/summoner/userName={summoner_name}}')
+    response = requests.get(f'https://na.op.gg/summoner/userName={summoner_name}')
+    # print(response)
     soup = BeautifulSoup(response.content, 'html.parser')
+    print(soup.get_text())
 
     game_result = soup.find_all('div', class_='GameResult')
-    win_loss = []
-    
-    for game in game_result:
-        
-        
-        win_loss.append(game.get_text().strip())
-    
-    await ctx.send(win_loss)
-    print(win_loss)
+    game_result_list = []
 
-    # dice = [
-    #     str(random.choice(range(1, number_of_sides + 1)))
-    #     for _ in range(number_of_dice)
-    # ]
-    # await ctx.send(', '.join(dice))
+    for game in game_result:
+        # print(game.get_text().strip())
+        game_result_list.append(game.get_text().strip())
+
+    #### parent class of champ_name is GameSettingInfo
+    game_setting_info = soup.find('div', class_ = 'GameItemList')
+    champ_name = game_setting_info.findChildren(class_='ChampionName')
+    champ_name_list = []
+
+    for champ in champ_name:
+        # print(champ.get_text().strip())
+        champ_name_list.append(champ.get_text().strip())
+
+    cs_score = game_setting_info.findChildren(class_='CS')
+    cs_list = []
+    for cs in cs_score:
+        # print(cs.get_text().strip())
+        cs = cs.get_text().strip()
+        # print(cs)
+        if cs[-1] == 'S':
+            cs_list.append(cs)
+    # print(cs_list)
+
+    merged_list = tuple(zip(game_result_list, champ_name_list, cs_list))
+    # print(merged_list)
+
+    for item in merged_list:
+        await ctx.send(item)
+
+    # await ctx.send(merged_list)
 
 bot.run(TOKEN)
--- Code/jesse_pena/project/opgg_test.py
+++ Code/jesse_pena/project/opgg_test.py
@@ -1,21 +1,47 @@
 import requests
 from bs4 import BeautifulSoup
 
+def champ_lookup():
+
+    summoner_name = input('Summoner name?')
+    response = requests.get(f'https://na.op.gg/summoner/userName={summoner_name}')
+
+    # response = requests.get('https://na.op.gg/summoner/userName=prismy')
+    soup = BeautifulSoup(response.content, 'html.parser')
 
 
-summoner_name = input('Summoner name?').lower
-response = requests.get('https://na.op.gg/summoner/userName={summoner_name}')
+    game_result = soup.find_all('div', class_='GameResult')
+    game_result_list = []
+    for game in game_result:
+        # print(game.get_text().strip())
+        game_result_list.append(game.get_text().strip())
 
-response = requests.get('https://na.op.gg/summoner/userName={summoner_name}}')
-soup = BeautifulSoup(response.content, 'html.parser')
-text = soup.get_text()
+    #### parent class of champ_name is GameSettingInfo
 
-game_result = soup.find_all('div', class_='GameResult')
-# print(game_item_wrap)
-for game in game_result:
-    print(game.get_text().strip())
+    game_setting_info = soup.find('div', class_ = 'GameItemList')
 
+    champ_name = game_setting_info.findChildren(class_='ChampionName')
+    champ_name_list = []
+    for champ in champ_name:
+        # print(champ.get_text().strip())
+        champ_name_list.append(champ.get_text().strip())
 
+    cs_score = game_setting_info.findChildren(class_='CS')
+    cs_list = []
+    for cs in cs_score:
+        # print(cs.get_text().strip())
+        cs = cs.get_text().strip()
+        # print(cs)
+        if cs[-1] == 'S':
+            cs_list.append(cs)
+    # print(cs_list)
 
+    
 
+    merged_list = tuple(zip(game_result_list, champ_name_list, cs_list))
+    print(merged_list)
+    return merged_list
 
+champ_lookup()
+# if __name__ == "__main__":
+#     champ_lookup('prismy')
