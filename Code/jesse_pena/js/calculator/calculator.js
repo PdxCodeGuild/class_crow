46dbe126d12eb840ca331c3d59ef6fa2986a1258
jpchato <jpchato@gmail.com>
2020-10-27 18:42:07 -0700

    vue calculator complete

--- Code/jesse_pena/js/calculator/calculator.js
+++ Code/jesse_pena/js/calculator/calculator.js
@@ -3,39 +3,51 @@ let calculator = new Vue({
     data:{
         numberString: '',
         formula: '',
+        displayValue: ''
+        
 
     },
     methods: {
         displayNumber: function(num){
             this.numberString += num
             // alert(this.numberString)
+            this.displayValue += num
             
         },
         sum: function(numberString){
             this.formula += `${numberString} +`
             // alert(this.formula)
             this.numberString = ''
+            this.displayValue = ''
         },
         subtract: function(numberString){
             this.formula += `${numberString} -`
             this.numberString = ''
+            this.displayValue = ''
         },
         divide: function(numberString){
             this.formula += `${numberString} /`
             this.numberString = ''
+            this.displayValue = ''
         },
         multiply: function(numberString){
             this.formula += `${numberString} *`
             this.numberString = ''
+            this.displayValue = ''
         },
-        execute: function(formula, numberString){
+        execute: function(formula){
             let answer = eval(formula + this.numberString)
-            alert(answer)
+            this.formula = ''
+            this.displayValue = answer
+            this.numberString = this.displayValue
+            
+            // alert(answer)
         },
-        allClear: function(formula, numberString){
+        allClear: function(){
             this.numberString = ''
-            this.formula =''
-            alert(numberString, formula)
+            this.formula = ''
+            this.displayValue = ''
+            // alert(numberString, formula)
         }
     }
 })
