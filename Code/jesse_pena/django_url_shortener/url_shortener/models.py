cfd690a29a5a852da07935ce6ce0e7002b456b1a
jpchato <jpchato@gmail.com>
2020-11-06 21:19:45 -0800

    added counter to model

--- Code/jesse_pena/django_url_shortener/url_shortener/models.py
+++ Code/jesse_pena/django_url_shortener/url_shortener/models.py
@@ -5,6 +5,8 @@ from django.db import models
 class Shortener(models.Model):
     shortened_url = models.CharField(max_length=255, blank=True, null=True)
     random_string_database = models.CharField(max_length=255, blank=True, null=True)
+    count = models.IntegerField(default = 0)
+    
 
     def __str__(self):
         return self.shortened_url
--- Code/jesse_pena/django_url_shortener/url_shortener/views.py
+++ Code/jesse_pena/django_url_shortener/url_shortener/views.py
@@ -8,8 +8,9 @@ import random
 
 def home(request):
     random_string = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(8))
+    shorteners = Shortener.objects.all()
     if request.method == 'GET':
-        shorteners = Shortener.objects.all()
+        
         context = {
             'shorteners': shorteners,
             'random_string': random_string
@@ -17,6 +18,8 @@ def home(request):
         return render (request, 'url_shortener/home.html', context)
     elif request.method == 'POST':
         shortened_url = request.POST['shortened_url']
+        # if Shortener.objects.filter(shorteners.shortened_url).exists():
+        #     shorteners.count += 1
         new_url = Shortener.objects.create(
             shortened_url = shortened_url,
             random_string_database = random_string
@@ -36,14 +39,11 @@ def list_view(request):
 
 def detail_view(request, id):
     shorteners = Shortener.objects.get(id=id)
-    # context = {
-    #     'shorteners': shorteners,
-    #     # 'random_string': random_string
-    # }
-
-    # context['data'] = Shortener.objects.get(id = id)
     website = 'http://' + shorteners.shortened_url
-    # console.log(shorteners)
-
-    # return render(request, 'url_shortener/detail_view.html', context)
     return redirect(website)
+
+# def counter_view(request, id):
+#     shorteners = Shortener.objects.get(id = id)
+#     if Shortener.objects.filter(shorteners.shortened_url).exists():
+#         shorteners.count += 1
+#     shorteners.save()
