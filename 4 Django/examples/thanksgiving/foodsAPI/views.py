b874e3193888056438f2298f9b354dad2191e4ae
sbtries <sarahbtracy17@gmail.com>
2020-11-24 17:58:20 -0800

    adding custom users

---
+++ 4 Django/examples/thanksgiving/foodsAPI/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from django.http.response import JsonResponse
+from .models import Food
+from .serializers import FoodSerializer
+
+@api_view(['GET', 'POST', 'DELETE'])
+def api_list(request):
+    if request.method == 'GET':
+        foods = Food.objects.all()
+        serializer = FoodSerializer(foods, many=True)
+        return Response(serializer.data)
+    if request.method == 'POST':
+        serializer = FoodSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        foods = Food.object.all().delete()
+        return JsonResponse({"message": "everything deleted, heeeyyy"})
